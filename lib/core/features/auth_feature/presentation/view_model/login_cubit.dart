a30ca072fcaf963eaedf55df00b12c44fb927617
Aya Nasser <ayanasser1853@gmail.com>
2026-06-25 17:54:46 +0300

    add sharedprfehelper class ,save token after login

--- lib/core/features/auth_feature/presentation/view_model/login_cubit.dart
+++ lib/core/features/auth_feature/presentation/view_model/login_cubit.dart
@@ -1,20 +1,28 @@
 import 'package:doc_doc/core/features/auth_feature/data/models/login_request.dart';
 import 'package:doc_doc/core/features/auth_feature/data/repositories/auth_repo.dart';
 import 'package:doc_doc/core/features/auth_feature/presentation/view_model/login_state.dart';
+import 'package:doc_doc/core/helper/share_pref_halper.dart';
+import 'package:doc_doc/core/helper/shared_pref_keys.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit(this.authRepo):super(LoginInitial());
-final AuthRepo authRepo; 
-  login(LoginRequest loginRequest)async{
+  LoginCubit(this.authRepo) : super(LoginInitial());
+  final AuthRepo authRepo;
+  login(LoginRequest loginRequest) async {
     emit(LoginLoading());
- final res=await authRepo.login(loginRequest);
+    final res = await authRepo.login(loginRequest);
 
-  res.fold(ifLeft: (f){
-emit(LoginFailure(f));
-  }, ifRight: (ifRight){
-    emit(LoginSuccess(ifRight));
-  });
+    res.fold(
+      ifLeft: (f) {
+        emit(LoginFailure(f));
+      },
+      ifRight: (ifRight) {
+        emit(LoginSuccess(ifRight));
+        SharedPrefHelper.setSecuredString(
+          SharedPrefKeys.userToken,
+          ifRight.data.token,
+        );
+      },
+    );
   }
-
 }
--- lib/core/networking/api_interceptor.dart
+++ lib/core/networking/api_interceptor.dart
@@ -1,9 +1,18 @@
 import 'package:dio/dio.dart';
+import 'package:doc_doc/core/helper/share_pref_halper.dart';
+import 'package:doc_doc/core/helper/shared_pref_keys.dart';
 
 class ApiInterceptor extends Interceptor {
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    // TODO: implement onRequest
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    options.headers['Authorization'] =
+        await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken) !=
+            null
+        ? 'Bearer ${await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken)}'
+        : null;
     super.onRequest(options, handler);
   }
 }
